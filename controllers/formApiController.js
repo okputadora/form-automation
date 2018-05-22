85bf6b71e6dd6393eea7e4a28112d0a65cc2e65e
okputadora <philipwisner@Davids-iMac.local>
2018-05-22 11:50:06 -0400

    create script to create form after authentication

--- controllers/formApiController.js
+++ controllers/formApiController.js
@@ -1,23 +1,13 @@
 const { google } = require('googleapis')
 
-
-// how would we get this form to others? the docs will probably tell us
-
 module.exports = {
-  authenticate: () => {
+  authenticate: (auth) => {
     return new Promise((resolve, reject) => {
       // authorize the teacher's account so we can make a form on their behalf
-      const oAuth2Client = new google.auth.OAuth2(
-        process.env.CLIENT_ID,
-        process.env.CLIENT_SECRET,
-        "http://localhost:3000/form",
-        process.env.API_KEY,
-      );
-  
       const SCOPES = ['https://www.googleapis.com/auth/script.projects'];
       const TOKEN_PATH = 'credentials.json';
   
-      const url = oAuth2Client.generateAuthUrl({
+      const url = auth.generateAuthUrl({
         // 'online' (default) or 'offline' (gets refresh_token)
         access_type: 'offline',
         // If you only need one scope you can pass it as a string
@@ -27,7 +17,13 @@ module.exports = {
       resolve(url);
     })
   },
-  create: (params) => {
+  create: (params, auth) => {
+    console.log(params)
+    return new Promise((resolve, reject) => {
+      const script = google.script({version: 'v1', auth})
+      let form = script.
+      resolve(form.getEditUrl())
+    })    
 
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const { google } = require('googleapis');
 const formApi = require('../controllers/formApiController')
+require('dotenv').config(); // strange that I need this here when it's already in app.js
+const oAuth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  "http://localhost:3000/form",
+  process.env.API_KEY,
+);
 const router = express.Router();
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  formApi.authenticate()
+  console.log(oAuth2Client)
+  formApi.authenticate(oAuth2Client)
   .then(redirectUrl => {
+    console.log("redirecting to ", redirectUrl)
     res.redirect(redirectUrl);
   })
   .catch(err => {
@@ -15,7 +28,23 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/form', function(req, res, next) {
-  res.render('form');
+  // code passed from google oAuth
+  const code = req.query.code
+  oAuth2Client.getToken(code, (err, token) => {
+      // if (err) return callback(err);
+      oAuth2Client.setCredentials(token);
+      res.render('form');    
+  });
+  
 })
+
+router.post('/makeForm', function(req, res, next) {
+  console.log("in the post route")
+  formApi.create(req.body, oAuth2Client)
+  .then(result => {
+      console.log(result)
+      res.send('respond with a resource');
+  })
+});
 
 module.exports = router;
